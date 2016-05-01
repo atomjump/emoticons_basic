92d5d844941d44872bd380b5d3bebd6802e1bc87
Peter Abrahamson <peter@atomjump.com>
2016-05-02 09:31:20 +1200

    first draft, with unmatching icons

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # emoticons_basic
 Plugin that adds basic emoticons to messages in the AtomJump Loop Server.
+
+## Installation
+
+Unzip or git clone into the folder: your-loop-server/plugins/emoticons_basic
+
+Add the string "emoticons_basic" into your-loop-server/config/config.json plugins array to enable the plugin. e.g. 
+
+     "plugins": [
+         "emoticons_basic"
+      ]
+      
+      
+## Using
+
+Enter 
+
+:)  for a smiley
+:(  for a frown
+;)  for a wink
+lol  for a 'laugh-out-loud'
+
+into your messages.
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+    include_once("classes/cls.pluginapi.php");
+
+    class plugin_emoticons_basic
+    {
+        public function before_message($message)
+        {
+            global $root_server_url;
+            
+            //Do your thing in here. Here is a sample.
+            $api = new cls_plugin_api();
+            
+
+            //e.g. hide the message we have just posted if it includes the string 'aargh' in it.
+            if(strpos($message, ':)') !== false) {
+                $message = str_replace(":)", '<img src="' . $root_server_url . '/plugins/emoticons_basic/img/smiley.png">', $message);
+            }
+            
+            if(strpos($message, ';)') !== false) {
+                $message = str_replace(";)", '<img src="' . $root_server_url . '/plugins/emoticons_basic/img/wink.png">', $message);
+            }
+            
+            if(strpos($message, ':(') !== false) {
+                $message = str_replace(":(", '<img src="' . $root_server_url . '/plugins/emoticons_basic/img/sad.png">', $message);
+            }
+            
+             if(strpos($message, 'lol') !== false) {
+                $message = str_replace("lol", '<img src="' . $root_server_url . '/plugins/emoticons_basic/img/lol.png">', $message);
+            }
+
+            return $message;
+
+        }
+    }
+?>
